ab563f7527b2add8adb86ba7ab92bb6e00782c2f
tanuja010101 <92984244+tanuja010101@users.noreply.github.com>
2022-11-23 10:26:33 +0530

    Add files via upload

---
+++ Springgit/src/main/java/com/Parwani/Springgit/SpringgitApplication.java
@@ -0,0 +1,13 @@
+package com.Parwani.Springgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgitApplication.class, args);
+	}
+
+}
